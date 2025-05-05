637f3c793f908483cc80921f6d02a73287d20060
Adryazman <161790588+adry-codelab@users.noreply.github.com>
2025-05-05 22:59:00 +0800

    first commit

---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss'
+})
+export class CounterComponent {
+  
+  counterValue = signal(0);
+
+  increase(){
+    //this.counterValue.set(this.counterValue() + 1)
+    this.counterValue.update((val) => val +1 );
+  }
+
+  decrease(){
+    this.counterValue.update((val) => val - 1);
+  }
+
+  reset(){
+    this.counterValue.set(0);
+  }
+}
